262427c0876497e03dcaf2ca1892d60312b1e816
LeDucHoang2002 <leduchoang.10022002@gmail.com>
2022-10-26 14:14:17 +0700

    firebase

--- app/src/main/java/com/example/leduchoang_2050531200150_baitapcuoiky/MainActivity.java
+++ app/src/main/java/com/example/leduchoang_2050531200150_baitapcuoiky/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -20,14 +22,18 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
-    List<Account> listacc;
-
-    TextView editText,editText2,sigup,logIn;
+    TextView email, pass,sigup,logIn;
     LinearLayout singUpLayout,logInLayout;
     Button singIn,btn_singUp;
     EditText eMails,passwordss,passwords01;
@@ -37,14 +43,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
 
-        listacc = new ArrayList<>();
-        listacc.add(new Account("admin","admin"));
-        listacc.add(new Account("huy","123"));
-        listacc.add(new Account("test","1"));
-
-        editText = (TextView) findViewById(R.id.eMail);
-        editText2 = (TextView) findViewById(R.id.passwords);
+        email = (TextView) findViewById(R.id.eMail);
+        pass = (TextView) findViewById(R.id.passwords);
 
         sigup =(TextView) findViewById(R.id.singUp);
         logIn =(TextView) findViewById(R.id.logIn);
@@ -62,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
         passwords01 = (EditText) findViewById(R.id.passwords01);
 
         sharedPreferences =getSharedPreferences("data", MODE_PRIVATE);
-        editText.setText(sharedPreferences.getString("taikhoan",""));
-        editText2.setText(sharedPreferences.getString("matkhau",""));
+        email.setText(sharedPreferences.getString("taikhoan",""));
+        pass.setText(sharedPreferences.getString("matkhau",""));
         cbRemember.setChecked(sharedPreferences.getBoolean("checked",false));
         sigup.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
@@ -93,94 +95,89 @@ public class MainActivity extends AppCompatActivity {
         singIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String uername =editText.getText().toString().trim();
-                String pass =editText2.getText().toString().trim();
-                if (checkpass()){
-                    Toast.makeText(MainActivity.this,"Đăng nhập thành công", Toast.LENGTH_SHORT).show();
-
-                    if(cbRemember.isChecked()){
-                        SharedPreferences.Editor editor=sharedPreferences.edit();
-                        editor.putString("taikhoan",uername);
-                        editor.putString("matkhau",pass);
-                        editor.putBoolean("checked",true);
-                        editor.commit();
-                    }else {
-                        SharedPreferences.Editor editor = sharedPreferences.edit();
-                        editor.remove("taikhoan");
-                        editor.remove("matkhau");
-                        editor.remove("checked");
-                        editor.commit();
-                    }
-                    Intent profile = new Intent(MainActivity.this,MainActivity2.class);
-                    startActivity(profile);
-                }
-                else {
-                    Toast.makeText(MainActivity.this,"Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+                login();
+                String uername =email.getText().toString().trim();
+                String pass = MainActivity.this.pass.getText().toString().trim();
+                if(cbRemember.isChecked()){
+                    SharedPreferences.Editor editor=sharedPreferences.edit();
+                    editor.putString("taikhoan",uername);
+                    editor.putString("matkhau",pass);
+                    editor.putBoolean("checked",true);
+                    editor.commit();
+                }else {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.remove("taikhoan");
+                    editor.remove("matkhau");
+                    editor.remove("checked");
+                    editor.commit();
                 }
             }
         });
         btn_singUp.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
-                boolean next =true;
-                if (eMails.getText().toString().equals("")
-                        || passwordss.getText().toString().equals("")
-                        || passwords01.getText().toString().equals(""))
-                {
-                    Toast.makeText(MainActivity.this,"Nhập đầy đủ thông tin",Toast.LENGTH_SHORT).show();
-                    next =false;
+                register();
+            }
+        });
+    }
+    private void login() {
+        String email1,pass1;
+        email1=email.getText().toString();
+        pass1=pass.getText().toString();
+        if (TextUtils.isEmpty(email1)){
+            Toast.makeText(this, "Vui Long Nhap Email", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(pass1)){
+            Toast.makeText(this, "Vui Long Nhap Password", Toast.LENGTH_SHORT).show();
+        }
+        if (email1.equals("")==false ||pass1.equals("")==false){
+            mAuth.signInWithEmailAndPassword(email1,pass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Dang Nhap Thanh Cong", Toast.LENGTH_SHORT).show();
+
+                        Intent i=new Intent(MainActivity.this,HomePage.class);
+                        startActivity(i);
+                    }else {
+                        Toast.makeText(MainActivity.this, "Dang Nhap Khong Thanh Cong", Toast.LENGTH_SHORT).show();
+                    }
                 }
+            });
+        }
 
-                if (next && !passwordss.getText().toString().equals(passwords01.getText().toString()))
-                {
-                    Toast.makeText(MainActivity.this,"Xác nhận sai",Toast.LENGTH_SHORT).show();
-                    next =false;
-                }
-
-                if (next)
-                {
-
-                    listacc.add(new Account(eMails.getText().toString(),passwordss.getText().toString()));
+    }private void register() {
+        String email1,pass1;
+        email1=eMails.getText().toString();
+        pass1=passwordss.getText().toString();
+        if (TextUtils.isEmpty(email1)){
+            Toast.makeText(this, "Vui Long Nhap Email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(pass1)){
+            Toast.makeText(this, "Vui Long Nhap Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email1,pass1).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                String uername =eMails.getText().toString().trim();
+                String password = passwordss.getText().toString().trim();
+                if (task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Tao Tai Khoan Thanh Cong", Toast.LENGTH_SHORT).show();
                     logIn.setBackground(getDrawable(R.drawable.switch_trcks));
                     logIn.setTextColor(getColor(R.color.white));
                     sigup.setBackground(getDrawable(R.drawable.switch_tumbs));
                     sigup.setTextColor(getColor(R.color.pinkColor));
                     logInLayout.setVisibility(View.VISIBLE);
                     singUpLayout.setVisibility(View.GONE);
-                    //finish();
+                    email.setText(uername);
+                    pass.setText(password);
+                }else {
+                    Toast.makeText(MainActivity.this, "Tao Tai Khoan Khong Thanh Cong", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-    }
-    private ActivityResultLauncher<Intent> getResult =registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK){
-                        Intent data = result.getData();
-                        Bundle dataacc =  data.getBundleExtra("bundle");
-                        Account newacc = (Account) dataacc.getSerializable("newacc");
-                        listacc.add(newacc);
-                        editText.setText(newacc.getUserName());
-                        editText2.setText(newacc.getPassWord());
-                    }
-                    if (result.getResultCode() == Activity.RESULT_CANCELED){
-                        Toast.makeText(MainActivity.this,"Bạn chưa tạo tài khoản",Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-    );
-    public boolean checkpass(){
-        boolean result = false;
-        for (Account acc : listacc) {
-            boolean check1 = String.valueOf(editText.getText()).equals(acc.getUserName());
-            boolean check2 = String.valueOf(editText2.getText()).equals(acc.getPassWord());
-            if (check1 && check2){
-                result = true;
-            }
-        }
-        return result;
     }
 }
